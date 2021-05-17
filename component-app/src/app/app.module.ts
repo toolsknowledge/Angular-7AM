483dafdaeb9ca5c04a04b3d962ef4eb838aec979
Samba <samba.pendela@gmail.com>
2021-05-17 07:51:43 +0530

    code

--- component-app/src/app/app.module.ts
+++ component-app/src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import firstComponent from './first.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    firstComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [firstComponent]
 })
 export class AppModule { }
