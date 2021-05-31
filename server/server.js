58790a203a015daf1ce1d5cd6da5aed5d66f3bc9
Samba <samba.pendela@gmail.com>
2021-05-31 07:59:39 +0530

    code

---
+++ server/server.js
@@ -0,0 +1,80 @@
+//import node modules
+//require() function, used to import the node modules
+const express = require("express");
+const mongodb = require("mongodb");
+const cors = require("cors");
+const bodyparser = require("body-parser");
+
+
+
+//rest object
+const app = express();
+//where "app" is the rest object
+//where "app" used to create the rest services Ex. GET,POST,PUT,DELETE,.....
+
+
+
+//enable cors policy
+app.use( cors() );
+
+
+
+
+
+
+//MIME Type
+app.use(bodyparser.json());
+
+
+
+//parse the client data
+app.use(bodyparser.urlencoded({extended:false}));
+
+
+
+//connect to database
+const miniproject = mongodb.MongoClient;
+//where "miniproject" is the refernce variable, used to connect to database
+
+
+
+//create the GET Request
+app.get("/products",(req,res)=>{
+    miniproject.connect("mongodb+srv://admin:admin@cluster0.jgnmk.mongodb.net/ngworkshop?retryWrites=true&w=majority",(err,connection)=>{
+        if(err) throw err;
+        else{
+            const db = connection.db("ngworkshop");
+            db.collection("products").find().toArray((err,array)=>{
+                if(err) throw err;
+                else{
+                    res.send(array);
+                }
+            });
+        }
+    });
+});
+
+
+//assign the port number
+let port = process.env.PORT || 8080; 
+app.listen(port,()=>{
+    console.log("serve started successfully !!!");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
